b713bffc9eebfd38bf5a9b6f6a72572f5a0392a6
akshaywritescode <teamakshayyadav@gmail.com>
2025-03-11 10:53:26 +0530

    added Navbar to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <header>
+        <Navbar />
+      </header>
     </>
   );
 }
