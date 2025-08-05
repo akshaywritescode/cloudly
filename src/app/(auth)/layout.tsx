9076f9ca1e49028f6ff297f7504b3edc53182891
akshaywritescode <teamakshayyadav@gmail.com>
2025-08-05 17:23:51 +0530

    wrapped auth layout in suspense

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,14 +1,17 @@
+import { Suspense } from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "../sections/Footer";
 
-export default function RootLayout({
+export default function AuthLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body>
         <Navbar />
-        {children}
+        <Suspense fallback={<div>Loading...</div>}>
+          {children}
+        </Suspense>
         <Footer />
       </body>
     </html>
