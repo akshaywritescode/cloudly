2a9cfd352bae843523e8a770f7e946d0a1298220
akshaywritescode <teamakshayyadav@gmail.com>
2025-09-19 16:56:34 +0530

    added storage functionality

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,15 +1,16 @@
 // lib/appwrite.ts
 
-import { Account, Client, Databases } from "appwrite";
+import { Account, Client, Databases, Storage } from "appwrite";
 
 // Load envs at build time — safe for both server and browser if NEXT_PUBLIC is used
 const ENDPOINT = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string;
 const PROJECT_ID = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string;
 const DEVKEY = process.env.NEXT_PUBLIC_APPWRITE_DEVKEY as string;
+const BUCKET_ID = process.env.NEXT_PUBLIC_APPWRITE_CLOUDLY_BUCKET_MAIN as string;
 
-if (!ENDPOINT || !PROJECT_ID || !DEVKEY) {
+if (!ENDPOINT || !PROJECT_ID || !DEVKEY || !BUCKET_ID) {
   throw new Error(
-    "Environment variables NEXT_PUBLIC_APPWRITE_ENDPOINT, NEXT_PUBLIC_APPWRITE_PROJECT_ID, and NEXT_PUBLIC_APPWRITE_DEVKEY must be defined"
+    "Environment variables NEXT_PUBLIC_APPWRITE_ENDPOINT, NEXT_PUBLIC_APPWRITE_PROJECT_ID, NEXT_PUBLIC_APPWRITE_DEVKEY, and NEXT_PUBLIC_APPWRITE_CLOUDLY_BUCKET_MAIN must be defined"
   );
 }
 
@@ -26,4 +27,12 @@ export function getAccount() {
 
 export function getDatabase() {
   return new Databases(getClient());
+}
+
+export function getStorage() {
+  return new Storage(getClient());
+}
+
+export function getBucketId() {
+  return BUCKET_ID;
 }
