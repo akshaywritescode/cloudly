9764ed751cd6dade5829db09547d811a8625ebc9
akshaywritescode <teamakshayyadav@gmail.com>
2025-10-30 10:27:15 +0530

    fix: made link work in landing page

--- src/app/sections/DiscoverSection.tsx
+++ src/app/sections/DiscoverSection.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { montserrat } from "../fonts";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export default function DiscoverSection() {
     return <section className="py-28 bg-white flex justify-center items-center">
@@ -9,7 +10,7 @@ export default function DiscoverSection() {
                 <h4 className={`px-3 m-auto text-white text-2xl tracking-wide text-center font-medium md:text-3xl lg:text-5xl lg:leading-[3.8rem] lg:w-[90%] ${montserrat.className}`}>Discover a better way to work together.</h4>
                 <p className="text-center px-5 mt-10 text-white text-sm md:text-base lg:text-base">Bring your entire workflow together on one integrated platform that works with the tools you already use. Edit PDFs, share videos, sign documents and collaborate seamlessly with internal and external stakeholders – all without leaving Cloudly.</p>
             </div>
-            <Button className="bg-white text-black py-6 text-base lg:py-8 lg:text-xl hover:bg-white/90 hover:cursor-pointer">Learn About Cloudly <ArrowRight /></Button>
+            <Link href={"/signup"}><Button className="bg-white text-black py-6 text-base lg:py-8 lg:text-xl hover:bg-white/90 hover:cursor-pointer">Learn About Cloudly <ArrowRight /></Button></Link>
         </div>
     </section>
 }
